ba5408324b8ad5e67d0ad9fb6d14e1da8f65c69a
ThunderInTheGrass <propeller8924@gmail.com>
2025-06-17 17:57:37 +0800

    20. NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import s from './Navbar.module.css';
+import { NavLink } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  console.log(s)
   return (
     <nav className={s.nav}>
       <ul className={s.list}>
-        {/* <li>
-          <Link className={s.item} to="/">Profile</Link>
+        <li>
+          <NavLink className={ ({isActive}) => isActive ? s.active : s.item } to="/">Profile</NavLink>
         </li>
         <li>
-          <Link className={s.item} href="/Dialogs">Messages</Link>
-        </li> */}
-        <li>
+          <NavLink className={ ({isActive}) => isActive ? s.active : s.item } to="/Dialogs">Messages</NavLink>
+        </li>
+        {/* <li>
           <a href="/Dialogs">Dialogs</a>
         </li>
         <li>
           <a href="/">Profile</a>
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
