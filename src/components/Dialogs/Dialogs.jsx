96fb2a711cffd285e4a7519c40e24bdc14ccc43d
ThunderInTheGrass <propeller8924@gmail.com>
2025-06-18 17:28:05 +0800

    Route exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = () => {
   return (
     <div className={s.dialogsWrapper}>
       <div className={s.dialogs}>
         <div className={s.dialog}>
-          Александр
+          <NavLink to="/Dialogs/1">Александр</NavLink>
         </div>
         <div className={s.dialog}>
-          Федор
+          <NavLink to="/Dialogs/2">Федор</NavLink>
         </div>
         <div className={s.dialog}>
-          Селиван
+          <NavLink to="/Dialogs/3">Селиван</NavLink>
         </div>
         <div className={s.dialog}>
-          Дед Вовка
+          <NavLink to="/Dialogs/4">Дед Вовка</NavLink>
         </div>
       </div>
       <div className={s.messages}>
